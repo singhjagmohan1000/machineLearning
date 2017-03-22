bdaf050245a7e09a87bde1be221d2e625925f5a6
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-22 02:27:29 -0700

    my first assignment machine learning

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-import sklearn
+from sklearn import tree
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels = [0,0,1,1]
+#decision tree classifier
+clf = tree.DecisionTreeClassifier()
+#fit is used to find  patterns in data.
+clf=clf.fit(features,labels)
+print  (clf.predict([[150,0]]))
