0badd54364f7838d6552c8f08a037e7fcd461dc7
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-22 03:10:07 -0700

    my second assignment machine learning

---
+++ secondIris.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+iris = load_iris()
+#print (iris.feature_names)
+#print (iris.target_names)
+#print (iris.data[0])
+#print (iris.target[0])
+test_idx=[0,50,100]
+
+#training data
+train_target=np.delete(iris.target,test_idx)
+train_data=np.delete(iris.data,test_idx,axis=0)
+
+#testing data should be kept secret from training data and classifier
+#testing data
+test_target=iris.target[test_idx]
+test_data=iris.data[test_idx]
+
+
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(train_data,train_target)
+
+print (test_target)
+
+print (clf.predict(test_data))
+
+
+#viz code
+
+# from sklearn.externals.six import StringIO
+# import pydotplus
+# dot_data = StringIO()
+# tree.export_graphviz(clf, out_file=dot_data,
+#                          feature_names=iris.feature_names,
+#                          class_names=iris.target_names,
+#                          filled=True, rounded=True,
+#                          impurity=False)
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+# graph.write_pdf("iris.pdf")
+
+print (test_data[0],test_target[0])
+
+
