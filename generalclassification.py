a2359b5243442df4d780a39becc248aece3ed6af
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-23 10:40:10 -0700

    deprecated warning resolved .....

--- generalclassification.py
+++ generalclassification.py
@@ -1,7 +1,9 @@
 from sklearn import tree
 
 # [height, weight, shoe_size]
-X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42],[181,85,43]]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40],
+     [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
@@ -10,6 +12,8 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 # decision tree classifier
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X,Y)
-prediction = clf.predict([190,70,43])
-print (prediction)
+clf = clf.fit(X, Y)
+
+validate = [[166, 65, 40]]
+prediction = clf.predict(validate)
+print(prediction)
