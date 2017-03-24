c2466ebc76287764366a2bc974efbb86744e632b
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-24 03:09:47 -0700

    Refining CSV Data for use

--- machineLearningUdacity/emptyDataFrame.py
+++ machineLearningUdacity/emptyDataFrame.py
@@ -12,10 +12,18 @@ def test_run():
 
     dfSPY = pd.read_csv("SPY.csv",index_col="Date",parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
 
+    dfSPY = dfSPY.rename(columns={'Adj Close': 'SPY'})
     #Join Two Dataframe
     df1=df1.join(dfSPY,how='inner')
-    print (df1)
+    #print (df1)
 
+    # Read more into Stocks
+    symbols = ['GOOG', 'IBM', 'GLD']
+    for symbol in symbols:
+        df_temp = pd.read_csv("{}.csv".format(symbol), index_col="Date", parse_dates=True,usecols=['Date','Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df1=df1.join(df_temp, how="left")
+    print (df1)
 
 
 
