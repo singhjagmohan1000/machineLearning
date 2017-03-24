d89ea4a11705f628751798426286e84d6b8e9c1b
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-24 02:29:08 -0700

    Refining CSV Data for use

--- machineLearningUdacity/emptyDataFrame.py
+++ machineLearningUdacity/emptyDataFrame.py
@@ -5,9 +5,15 @@ def test_run():
     start_date = '2010-01-22'
     end_date = '2010-01-26'
     dates = pd.date_range(start_date, end_date)
-    print (dates)
-    print (dates[0])
+
+
+    #Empty Data Frame
     df1 = pd.DataFrame(index = dates)
+
+    dfSPY = pd.read_csv("SPY.csv",index_col="Date",parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
+
+    #Join Two Dataframe
+    df1=df1.join(dfSPY,how='inner')
     print (df1)
 
 
