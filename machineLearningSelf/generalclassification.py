d8a55f880d953f34b4a7b00f3566d8d2304e366d
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-24 01:44:12 -0700

    Restructuring Directories and start using pandas library

--- machineLearningSelf/generalclassification.py
+++ machineLearningSelf/generalclassification.py
@@ -1,4 +1,4 @@
-from sklearn import tree
+from sklearn import tree,metrics
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
@@ -14,6 +14,19 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
 
-validate = [[166, 65, 40]]
+validate = [[189, 65, 48]]
 prediction = clf.predict(validate)
 print(prediction)
+print(metrics.accuracy_score(['male'], prediction))
+
+
+## Nearest Neighbour
+
+from sklearn.neighbors import KNeighborsClassifier
+
+clf1 = KNeighborsClassifier()
+clf1 = clf1.fit(X, Y)
+prediction1 = clf1.predict(validate)
+print(prediction1)
+print(metrics.accuracy_score(['male'], prediction1))
+
